53403f8788b96371645678643514766f46e92f4a
Javier <xvrgonzalez21@gmail.com>
2026-07-04 16:24:40 -0600

    edirted urls for audio callls, edited django_settings_module env var for test env

--- call_analyzer_api/audiocalls/urls.py
+++ call_analyzer_api/audiocalls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "audiocalls"
 urlpatterns = [
+    path("", views.ListCallsView.as_view(), name="list_calls"),
     path("upload/", views.UploadAudioFileView.as_view(), name="upload_file"),
-    path("list/", views.ListCallsView.as_view(), name="list_calls"),
-    path("details/<str:call_id>/", views.CallDetailsView.as_view(), name="call_detail"),
+    path("<str:call_id>/", views.CallDetailsView.as_view(), name="call_detail"),
 ]
