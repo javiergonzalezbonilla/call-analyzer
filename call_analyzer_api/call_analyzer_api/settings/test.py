c187817e36536c3127b2a1d01d6d4a8bc6cda3a4
Javier <xvrgonzalez21@gmail.com>
2026-07-04 18:48:48 -0600

    added test config file

---
+++ call_analyzer_api/call_analyzer_api/settings/test.py
@@ -0,0 +1,10 @@
+from .base import env
+from .local import *  # noqa
+
+STT_PROVIDER = "mock"
+DEEPGRAM_API_KEY = ""
+
+AWS_ACCESS_KEY_ID = "test"
+AWS_SECRET_ACCESS_KEY = "test"
+AWS_S3_ENDPOINT_URL = "http://nginx:9000"
+AWS_UPLOADED_FILES_BUCKET_NAME = "uploadedfiles"
